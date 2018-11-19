da6836349d638fd7359a69ff8498741251be5f4b
Kautsky Lozano <kautskylozanov@outlook.com>
2018-11-19 08:44:55 +0100

    Initial commit

--- Angular7App/ClientApp/src/app/app.component.ts
+++ Angular7App/ClientApp/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { DragDropModule, CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+
+@NgModule({
+  imports: [BrowserAnimationsModule, DragDropModule]
+})
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  styleUrls: ['./app.component.scss']
+  })
+
+
 export class AppComponent {
-  title = 'app';
+  title = 'ClientApp';
+  timePeriods = [
+    'Bronze age',
+    'Iron age',
+    'Middle ages',
+    'Early modern period',
+    'Long nineteenth century'
+  ];
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.timePeriods, event.previousIndex, event.currentIndex);
+  }
+
 }
+
+
